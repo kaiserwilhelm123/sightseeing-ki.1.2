aec4ffeb9a6f23239e1a1535887b8e10b1b75e7f
kaiserwilhelm123 <willipaul.s@hbg.schule.koeln>
2026-03-09 10:19:30 +0100

    Add attractions data for Köln, Berlin, and Paris

---
+++ attractions.js
@@ -0,0 +1,15 @@
+const attractions = [
+
+{name:"Kölner Dom", city:"köln", category:"kirche", time:90, popularity:10},
+{name:"Museum Ludwig", city:"köln", category:"museum", time:120, popularity:8},
+{name:"Schokoladenmuseum", city:"köln", category:"museum", time:90, popularity:9},
+{name:"Hohenzollernbrücke", city:"köln", category:"denkmal", time:30, popularity:7},
+
+{name:"Brandenburger Tor", city:"berlin", category:"denkmal", time:30, popularity:10},
+{name:"Pergamonmuseum", city:"berlin", category:"museum", time:120, popularity:9},
+{name:"Berliner Dom", city:"berlin", category:"kirche", time:60, popularity:8},
+
+{name:"Eiffelturm", city:"paris", category:"denkmal", time:90, popularity:10},
+{name:"Louvre", city:"paris", category:"museum", time:180, popularity:10}
+
+];
